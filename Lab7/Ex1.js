ef56e34ff87f09998500db571bf08acbd28f4f57
Emma Sharp <131301752+emma-sharp@users.noreply.github.com>
2023-09-29 09:59:53 -1000

    lab7

---
+++ Lab7/Ex1.js
@@ -0,0 +1,9 @@
+require("./products_data.js");
+
+let num_products = 5;
+let product_num = 1;
+while(product_num <= num_products) {
+    console.log(prod_number + '. ' + eval('name' + product_num));
+    product_num++;
+}
+console.log(`That's all we have!`);
