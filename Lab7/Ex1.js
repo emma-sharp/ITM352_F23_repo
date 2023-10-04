885d0875f5b37cc0a4de397227aa8b79c14eb2e8
Emma Sharp <131301752+emma-sharp@users.noreply.github.com>
2023-10-03 14:53:09 -1000

    lab7

--- Lab7/Ex1.js
+++ Lab7/Ex1.js
@@ -1,9 +1,11 @@
 require("./products_data.js");
 
-let num_products = 5;
 let product_num = 1;
-while(product_num <= num_products) {
-    console.log(prod_number + '. ' + eval('name' + product_num));
+while (eval("typeof name" + product_num) !== 'undefined') {
+    console.log(product_num + '. ' + eval('name' + product_num));
     product_num++;
 }
+
 console.log(`That's all we have!`);
+
+
