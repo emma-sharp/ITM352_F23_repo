463fb08f0f9cabad34c413c450aff7d256ecb1c9
Emma Sharp <131301752+emma-sharp@users.noreply.github.com>
2023-10-12 23:09:59 -1000

    SPP3 WOD

---
+++ WODs/SmartPhoneProducts3/products_data.js
@@ -0,0 +1,13 @@
+let num_items = 5;
+
+const product1 = { brand: "HTC", price: 40.00, image: "http://dport96.github.io/ITM352/morea/080.flow-control-II/HTC.jpg"};
+
+const product2 = { brand: "Apple", price: 75.00, image: "http://dport96.github.io/ITM352/morea/080.flow-control-II/iphone-3gs.jpg"};
+
+const product3 = { brand: "Nokia", price: 35.00, image: "http://dport96.github.io/ITM352/morea/080.flow-control-II/Nokia.jpg"};
+
+const product4 = { brand: "Samsung", price: 45.00, image: "http://dport96.github.io/ITM352/morea/080.flow-control-II/Samsung.jpg"};
+
+const product5 = { brand: "Blackberry", price: 10.00, image: "http://dport96.github.io/ITM352/morea/080.flow-control-II/Blackberry.jpg"};
+
+const products = [product1, product2, product3, product4, product5];
