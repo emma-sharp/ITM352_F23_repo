ec93199b018698821a7cc71cd12b78433d47abf1
Emma Sharp <131301752+emma-sharp@users.noreply.github.com>
2023-11-14 10:59:59 -1000

    ass. 1

---
+++ Sharp_Emma_Assignment1/js/products.js
@@ -0,0 +1,43 @@
+[
+    {
+      "name": "Classic Shortboard",
+      "type": "Shortboard",
+      "price": 450.00,
+      "description": "Used shortboard, great for advanced surfers tackling larger waves. Some minor dings, fully repaired.",
+      "image": "ClassicShortboard.jpg",
+      "quantity_available": 3
+    },
+    {
+      "name": "Sunset Longboard",
+      "type": "Longboard",
+      "price": 550.00,
+      "description": "Gently used longboard, perfect for beginners. Stable and easy to ride.",
+      "image": "SunsetLongboard.jpg",
+      "quantity_available": 2
+    },
+    {
+      "name": "Retro Fish",
+      "type": "Fish",
+      "price": 400.00,
+      "description": "Vintage fish surfboard, ideal for small to medium waves. Very good condition.",
+      "image": "RetroFish.jpg",
+      "quantity_available": 5
+    },
+    {
+      "name": "Hybrid Funboard",
+      "type": "Funboard",
+      "price": 480.00,
+      "description": "A versatile funboard, suitable for a wide range of wave sizes. Excellent for intermediate surfers.",
+      "image": "HybridFunboard.jpg",
+      "quantity_available": 4
+    },
+    {
+      "name": "Big Wave Gun",
+      "type": "Gun",
+      "price": 600.00,
+      "description": "Designed for big wave surfing. Long, sleek, and fast. In excellent condition.",
+      "image": "BigWaveGun.jpg",
+      "quantity_available": 1
+    }
+  ]
+  
