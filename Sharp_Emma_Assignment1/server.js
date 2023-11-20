f14491ec50b39af0d66a520cff9673d7907596bb
Emma Sharp <131301752+emma-sharp@users.noreply.github.com>
2023-11-19 20:01:23 -1000

    Assignment 1 edits

--- Sharp_Emma_Assignment1/server.js
+++ Sharp_Emma_Assignment1/server.js
@@ -1,9 +1,43 @@
-const products_array = require(__dirname + '/product_data.json');
-
 const express = require('express');
 const app = express();
+var product_data = require(__dirname + "/product_data.json");
+const qs = require('querystring');
+
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get("/test", function (request, response) {
+   response.status(200).send(products);
+})
 
 // Routing 
+app.get("/product_data.js", function (request, response, next) {
+   response.type('.js');
+   let products_str = `var products = ${JSON.stringify(product_data)};`;
+   response.send(products_str);
+});
+
+app.post("/purchase", function (request, response) {
+   console.log(request.body);
+
+   let errors = {};
+   for(let i in product_data) { 
+      let qty = request.body['product' + i];
+      // validate quantities
+      
+      // check if nonnegint
+      if (!isNonNegInt(qty)) {  errors['product'+i] = isNonNegInt(qty, true); }
+      if (!isInStock(qty, product_data[i].quantity_available)) { errors['product' + i] += "Out of Stock"; }
+
+   }
+   var qstr = qs.stringify(request.body);
+   if (Object.entries(errors).length === 0) { response.redirect(`invoice.html?${qstr}`); }
+   else { response.redirect(`product_display.html?${qstr}`); }
+});
+
+app.post("/order", function (request, response) {
+   response.redirect("order_submitted.html");
+})
 
 // monitor all requests
 app.all('*', function (request, response, next) {
@@ -12,10 +46,34 @@ app.all('*', function (request, response, next) {
 });
 
 // process purchase request (validate quantities, check quantity available)
-// <** your code here **>
+
 
 // route all other GET requests to files in public 
 app.use(express.static(__dirname + '/public'));
 
 // start server
-app.listen(8080, () => console.log(`listening on port 8080`));
+const PORT = process.env.PORT || 8080
+
+app.listen(PORT, () => {
+  console.log(`test app listening on port ${PORT}`)
+})
+
+function isNonNegInt(value, returnErrors = false) {
+   errors = []; // assume no errors at first
+   if(value === '') {
+       value = 0;
+   }
+   if (Number(value) != value) {
+       errors.push('Not a number!'); // Check if string is a number value
+   }
+   else {
+       if (value < 0) errors.push('Negative value!'); // Check if it is non-negative
+       if (parseInt(value) != value) errors.push('Not an integer!'); // Check that it is an integer
+   }
+
+   return returnErrors ? errors : (errors.length == 0);
+}
+
+function isInStock(value, stock) {
+   if (value > stock) { return false; }
+}
